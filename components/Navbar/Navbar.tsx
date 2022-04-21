0b025c032f57d7b85e5a55de875e0aad46163654
Matheus <matheuscmanoel@gmail.com>
2022-04-20 23:39:57 -0300

    feat: add nav and header

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export const Navbar = (): JSX.Element => {
+  return (
+    <nav>
+      <ul className="flex flex-row text-2xl text-strong-brown">
+        <li>
+          <Link href="/">
+            <a className="mr-10">home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a className="mr-10">auctions</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a className="mr-10">wtf</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>opensea</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-yellow": "#FEEFDD",
+        "strong-brown": "#201E1F",
+      },
+    },
   },
   plugins: [],
 };
